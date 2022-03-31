0abd38eed8314e36b4a6151442034df0ccfc33db
Viktor Kocur <kocurvik@gmail.com>
2022-03-31 23:55:13 +0200

    add two separate ensamble scripts

--- ensemble_s1.py
+++ ensemble_s1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-distmat_paths = ['./logs/stage2/lftd/v1/dist_mat.npy']
+distmat_paths = ['./logs/stage1/lftd/dist_mat.npy']
 
 # method 1
 distmat = np.zeros((1103,31238))
@@ -19,7 +19,7 @@ sort_distmat_index = np.argsort(distmat, axis=1)
 print(sort_distmat_index)
 print('The shape of distmat is: {}'.format(distmat.shape))
 
-save_path = './track_s2.txt'
+save_path = './track_s1.txt'
 with open(save_path,'w') as f:
     for item in sort_distmat_index:
         for i in range(99):
