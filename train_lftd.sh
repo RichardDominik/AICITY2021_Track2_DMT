849c12ac2ac386eb6e96af3d844038f7c046a098
Viktor Kocur <kocurvik@gmail.com>
2022-04-01 19:50:24 +0200

    lftd stage 1 + stage 2 training script

---
+++ train_lftd.sh
@@ -0,0 +1,7 @@
+python train.py --config_file=configs/stage1/lftd.yml
+# python test.py --config_file configs/stage1/lftd.yml MODEL.DEVICE_ID "('0')" TEST.WEIGHT './logs/stage1/lftd/lftd_10.pth' OUTPUT_DIR './logs/stage1/lftd/'
+
+python train_stage2_v1.py --config_file=configs/stage2/lftd.yml
+python test.py --config_file configs/stage2/lftd.yml MODEL.DEVICE_ID "('0')" TEST.WEIGHT './logs/stage2/lftd/v1/lftd_3.pth' OUTPUT_DIR './logs/stage2/lftd/v1/'
+python ensamble.py
+
